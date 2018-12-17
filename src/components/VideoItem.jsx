ffa3d132bf02009ab6c59b9bec5b1e69fabcae31
Denis <ddujota@internationalsafety.com>
2018-12-16 23:45:28 -0500

    centered the tecxt and reduced image size

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,9 +1,10 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({ video }) => {
   const { title, thumbnails } = video.snippet;
   return (
-    <div className="item">
+    <div className="video-item item">
       <img className="ui image" src={thumbnails.medium.url} alt={title} />
       <div className="content">
         <div className="header">{title}</div>
