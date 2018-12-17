55806c12bfe60f84fe84f98c8c4503e434976c96
Denis <ddujota@internationalsafety.com>
2018-12-16 23:05:40 -0500

    fixed bug that was cuasing a 403 on the api call

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyCayHHWo-Rrp0f0SWCVVfaUCh-zXrYnNYU';
+const KEY = 'AIzaSyB2JEIFZGWPx63VTohbIZeyR8DJDbrMiSQ';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
     maxResults: '5',
-    keys: KEY
+    key: KEY
   }
 });
