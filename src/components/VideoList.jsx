0decc5bcd33af7bc8b169733962e7ed7d13787e3
Denis <ddujota@internationalsafety.com>
2018-12-16 23:27:41 -0500

    refactored the video list and created a mmap of response that will render the video item component that now takes in a key and video prop

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
-  return (
-    <div>
-      {videos.map(video => {
-        <VideoItem />;
-      })}
-    </div>
-  );
+  // first map over the array result and then save it to a variable
+  const renderedList = videos.map(video => {
+    console.log(video);
+    return <VideoItem key={video.id.videoId} video={video} />;
+  });
+
+  // Then we render to the page by passing the variable result to the return statement
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
